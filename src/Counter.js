ed40bb96ece98094bea9824230c71afbcfca492f
Maycon Beserra <mayconbeserra@hotmail.com>
2018-11-28 12:33:07 +0100

    first exercises

---
+++ src/Counter.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCounter] = useState(0);
+
+  console.log('render');
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCounter(count + 1)}>increase</button>
+      <button onClick={() => setCounter(count - 1)}>decrease</button>
+    </div>
+  );
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './styles.css';
+
+//components
+// import Counter from './Counter';
+
+function Counter() {
+    const [count, setCounter] = useState(0);
+
+    console.log('render');
+
+    return (
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={() => setCounter(count + 1)}>increase</button>
+        <button onClick={() => setCounter(count - 1)}>decrease</button>
+      </div>
+    );
+  }
+
+function useToggle(initialValue) {
+  const [value, setToggle] = useState(false);
+
+  return {
+    value,
+    inverse: () => setToggle(!value)
+  }
+}
+
+function App() {
+  const toggle = useToggle(false);
+
+  return (
+    <div>
+      <h2>Toggle it?</h2>
+      <button onClick={toggle.inverse}>
+        {`${toggle ? 'Hide it' : 'Show'} counter`}
+      </button>
+      { toggle.value && <Counter /> }
+    </div>
+  );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
